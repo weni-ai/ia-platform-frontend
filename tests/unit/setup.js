bfd563595bd72ba5c691cccb11252e37e0ab1514
lucia_vulcano <vulcanolucia@gmail.com>
2022-01-12 15:11:15 -0300

    test

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -6,18 +6,6 @@ Vue.use(Buefy);
 
 Vue.config.productionTip = false;
 
-jest.mock('../../public/config.js', () => ({
-  VUE_APP_SUPPORTED_LANGUAGES: 'en|pt',
-  VUE_APP_VERSION: '0.0.0',
-  VUE_APP_BOTHUB_NLP_BASE_URL: 'http://localhost:2657/',
-  VUE_APP_BOTHUB_WEBAPP_BASE_URL: 'http://localhost:8080/',
-
-  get(name){
-    return this[name] || process.env[name]
-  }
-
-}));
-
 
 MockDate.set(1534341842684);
 
--- tests/unit/specs/components/repository/repository-evaluate/example/NewEvaluateExample.spec.js
+++ tests/unit/specs/components/repository/repository-evaluate/example/NewEvaluateExample.spec.js
@@ -1,7 +1,17 @@
 
 /* eslint-disable import/first */
 jest.mock('@/api/request');
+jest.mock('../../../../../../../public/config.js', () => ({
+  VUE_APP_SUPPORTED_LANGUAGES: 'en|pt',
+  VUE_APP_VERSION: '0.0.0',
+  VUE_APP_BOTHUB_NLP_BASE_URL: 'http://localhost:2657/',
+  VUE_APP_BOTHUB_WEBAPP_BASE_URL: 'http://localhost:8080/',
 
+  get(name){
+    return this[name] || process.env[name]
+  }
+
+}));
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Vuex from 'vuex';
 import NewEvaluateExample from '@/components/repository/repository-evaluate/example/NewEvaluateExample';
