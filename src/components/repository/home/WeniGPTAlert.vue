d05dfbd252d3a51a447f253f06d6d29bb83e84c7
Matheus Cristian <mathues@email.com>
2024-01-02 16:01:57 -0300

    feat: added close functionality to warning about news

--- src/components/repository/home/WeniGPTAlert.vue
+++ src/components/repository/home/WeniGPTAlert.vue
@@ -7,42 +7,33 @@
         display: canShow ? null : 'none',
       }"
     >
-      <div class="is-flex is-align-items-center mx-auto">
-          <img
-            src="~@/assets/imgs/doris.png"
-            width="50px"
-            class="mr-2"
-          />
+      <div class="warning-bar__content is-flex is-align-items-center mx-auto">
+        <img
+          class="doris-face"
+          src="~@/assets/imgs/doris.png"
+          width="50px"
+        />
 
           <span>
             Chegou o novo módulo de Inteligência Artificial!
             Explore as novidades e saiba como criar chatbots ainda mais incríveis!
           </span>
       </div>
-      <div>
-          <unnnic-icon-svg icon="close-1" size="sm" clickable @click="canShow = false" />
-      </div>
+
+      <unnnic-icon-svg icon="close-1" size="sm" clickable @click="canShow = false" />
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
-
 export default {
   name: 'WeniGPTAlert',
 
   data() {
     return {
+      canShow: this.$route.name === 'home',
     };
   },
-
-  computed: {
-    canShow() {
-      return this.$route.name === 'home';
-    },
-  },
-
 };
 </script>
 
@@ -50,17 +41,29 @@ export default {
 @import '~@weni/unnnic-system/src/assets/scss/unnnic.scss';
 
 .warning-bar {
+  position: relative;
   background-color: $unnnic-color-aux-blue-100;
   color: $unnnic-color-neutral-darkest;
   font-family: $unnnic-font-family-secondary;
   font-weight: $unnnic-font-weight-bold;
   font-size: $unnnic-font-size-body-lg;
+  line-height: $unnnic-font-size-body-lg + $unnnic-line-height-md;
   padding: $unnnic-spacing-sm;
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 10px;
-  line-height: 0;
-  height: 56px;
+  gap: $unnnic-spacing-xs;
+  min-height: $unnnic-font-size * 3.5;
+  box-shadow: $unnnic-shadow-level-near;
+
+  &__content {
+    padding-left: 50px + $unnnic-spacing-xs;
+
+    .doris-face {
+      position: absolute;
+      margin-left: -50px - $unnnic-spacing-xs;
+    }
+  }
+
 }
 </style>
