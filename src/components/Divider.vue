6886ba40fcc57e755dd0be070a4e9f5bf6c1a27e
Matheus Cristian <mathues@email.com>
2024-02-09 14:56:09 -0300

    feat: added y spacing prop to divider

--- src/components/Divider.vue
+++ src/components/Divider.vue
@@ -1,6 +1,17 @@
 <template>
-  <hr class="divider">
+  <hr :class="['divider', `divider--y-spacing-${ySpacing}`]">
 </template>
+
+<script>
+export default {
+  props: {
+    ySpacing: {
+      type: String,
+      default: 'md',
+    },
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 @import "~@weni/unnnic-system/src/assets/scss/unnnic.scss";
@@ -10,5 +21,11 @@
   height: $unnnic-border-width-thinner;
   background-color: $unnnic-color-neutral-soft;
   margin-block: ($unnnic-spacing-stack-md - $unnnic-border-width-thinner) $unnnic-spacing-stack-md;
+
+  &--y-spacing-lg {
+    margin-block:
+      ($unnnic-spacing-stack-lg - $unnnic-border-width-thinner)
+      $unnnic-spacing-stack-lg;
+  }
 }
 </style>
