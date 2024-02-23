781e32bf5ef619002e95492c2d3ab7b4d74a18c8
Matheus Cristian <mathues@email.com>
2024-02-23 15:59:42 -0300

    feat: adds Weni eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,18 @@
 module.exports = {
-  "root": true,
-  "env": {
-    "node": true
+  root: true,
+  env: {
+    node: true,
   },
-  "extends": [
-    "plugin:vue/essential",
-    "eslint:recommended"
-  ],
-  "parserOptions": {
-    "parser": "@babel/eslint-parser"
+  extends: ['@weni/eslint-config/vue2'],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
   },
-  "rules": {
+  rules: {
     'vue/multi-word-component-names': 'off',
     'vue/no-unused-components': 'off',
     'vue/no-mutating-props': 'off',
     'vue/valid-next-tick': 'off',
     'no-unused-vars': 'off',
-    'no-undef': 'off'
-  }
+    'no-undef': 'off',
+  },
 };
