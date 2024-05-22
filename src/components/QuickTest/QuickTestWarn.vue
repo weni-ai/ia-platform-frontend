480d1724df246f08c4a98986c9af78bce382aa34
Matheus Cristian <mathues@email.com>
2024-05-22 15:30:28 -0300

    feat(brain-global-save-button): adds need to save the changes warning

---
+++ src/components/QuickTest/QuickTestWarn.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="quick-test-warn">
+    <UnnnicIcon
+      :icon="icon"
+      scheme="inherit"
+      size="sm"
+    />
+
+    <UnnnicIntelligenceText
+      family="secondary"
+      size="body-gt"
+      color="inherit"
+    >
+      {{ text }}
+    </UnnnicIntelligenceText>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: String,
+    text: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '~@weni/unnnic-system/src/assets/scss/unnnic.scss';
+
+.quick-test-warn {
+  display: flex;
+  align-items: center;
+  column-gap: $unnnic-spacing-xs;
+  background-color: $unnnic-color-aux-yellow-100;
+  color: $unnnic-color-aux-yellow-700;
+  padding: $unnnic-spacing-ant;
+  border-radius: $unnnic-border-radius-sm;
+}
+</style>
