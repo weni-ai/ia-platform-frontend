6968cbe75be92f667c2ac427a8b453412da1a7b1
Marcus Vinícius <marcusvinici25@gmail.com>
2024-05-22 15:02:16 -0300

    adjusting order inputs and adding tooltip in routerTunings

--- src/views/repository/content/router/RouterTunings.vue
+++ src/views/repository/content/router/RouterTunings.vue
@@ -76,20 +76,45 @@
         tag="div"
         height="46px"
       />
-      <UnnnicFormElement
-        class="tunings__container_fields-element"
-        :label="$t(`router.tunings.fields.${field.name}`)"
-        v-if="field.type === 'password'"
-        :key="index"
+      <header
+        v-if="['token'].includes(field.name)"
+        :key="`label-${index}`"
+        class="tunings__form-element__label"
       >
-        <UnnnicInput
-          :value="field.value"
-          @input="$set(values, field.name, $event)"
-          :type="hasValidate ? 'error' : 'normal'"
-          :message="hasValidate ? $t('customization.invalid_field') : ''"
-          :nativeType="field.type"
-        />
-      </UnnnicFormElement>
+        <UnnnicIntelligenceText
+          color="neutral-cloudy"
+          family="secondary"
+          size="body-gt"
+          tag="p"
+        >
+          {{ $t(`router.tunings.fields.${field.name}`) }}
+        </UnnnicIntelligenceText>
+
+        <UnnnicToolTip
+          v-if="['token'].includes(field.name)"
+          side="right"
+          :text="$t(`router.tunings.fields.token_info`)"
+          enabled
+          maxWidth="29rem"
+          class="tunings__form-element__label__tooltip"
+        >
+          <UnnnicIcon
+            icon="info"
+            size="sm"
+            scheme="neutral-cleanest"
+            filled
+          />
+        </UnnnicToolTip>
+      </header>
+      <UnnnicInput
+        v-if="['token'].includes(field.name)"
+        :key="index"
+        :value="field.value"
+        @input="$set(values, field.name, $event)"
+        :type="hasValidate ? 'error' : 'normal'"
+        :message="hasValidate ? $t('customization.invalid_field') : ''"
+        :nativeType="field.type"
+      />
     </template>
 
     <RouterTuningsAdvanced
@@ -261,14 +286,14 @@ export default {
           name: 'ChatGPT',
           fields: [
             {
-              type: 'password',
-              name: 'token',
-            },
-            {
               type: 'select',
               name: 'version-gpt',
               default: 'gpt-4o',
               options: ['gpt-3.5-turbo', 'gpt-4-turbo', 'gpt-4o'],
+            },
+            {
+              type: 'password',
+              name: 'token',
             },
             {
               type: 'select',
