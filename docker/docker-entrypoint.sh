650756d674d8b5cade98af94f1ad90ab17f93938
Matheus Cristian <mathues@email.com>
2024-05-23 17:39:21 -0300

    feat: changes way to expose the env vars

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-if [ ! -f /usr/share/nginx/html/bothub-webapp/config.js ] ; then
-	envsubst < /usr/share/nginx/html/bothub-webapp/config.js.tmpl > /usr/share/nginx/html/bothub-webapp/config.js
-fi
+export JSON_STRING='window.runtimeVariables = (() => ({\
+  VITE_SUPPORTED_LANGUAGES: "'${VITE_SUPPORTED_LANGUAGES}'",\
+  VITE_BOTHUB_WEBAPP_USE_SENTRY: "'${VITE_BOTHUB_WEBAPP_USE_SENTRY}'",\
+  VITE_BOTHUB_WEBAPP_USE_HOTJAR: "'${VITE_BOTHUB_WEBAPP_USE_HOTJAR}'",\
+  VITE_API_BASE_URL: "'${VITE_API_BASE_URL}'",\
+  SENTRY_ENVIRONMENT: "'${SENTRY_ENVIRONMENT}'",\
+  NEXUS_API_BASE_URL: "'${NEXUS_API_BASE_URL}'",\
+  FLOWS_API_BASE_URL: "'${FLOWS_API_BASE_URL}'",\
+  get(name){\
+    return this[name];\
+  }\
+}))();\
+}'
+
+sed "s|\/\/CONFIGURATIONS_PLACEHOLDER|${JSON_STRING}|" /usr/share/nginx/html/bothub-webapp/index.html.tmpl > /tmp/index.html
 
 exec "$@"
