a68d762f27891c9b6ab0619a6e3d7d01da13974a
nataliaweni <natalia.assis@weni.ai>
2024-05-27 16:52:22 -0300

    Fix dockerfile

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ ! -f /usr/share/nginx/html/intelligence-webapp/config.js ] ; then
-	envsubst < /usr/share/nginx/html/intelligence-webapp/config.js.tmpl > /usr/share/nginx/html/intelligence-webapp/config.js
+if [ ! -f /usr/share/nginx/html/bothub-webapp/config.js ] ; then
+	envsubst < /usr/share/nginx/html/bothub-webapp/config.js.tmpl > /usr/share/nginx/html/bothub-webapp/config.js
 fi
 
 exec "$@"
