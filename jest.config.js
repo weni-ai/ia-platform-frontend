a169fa4f711981342e74ef5cd98897ca789cd4da
Marcus Vinícius <marcusvinici25@gmail.com>
2024-06-04 09:09:19 -0300

    adjusting jest setup and packages

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,15 @@
 /** @returns {Promise<import('jest').Config>} */
 
+process.env.VUE_APP_SUPPORTED_LANGUAGES = 'en|pt';
+process.env.VUE_APP_VERSION = '0.0.0';
+process.env.VUE_APP_BOTHUB_NLP_BASE_URL = 'http://localhost:2657/';
+process.env.VUE_APP_BOTHUB_WEBAPP_BASE_URL = 'http://localhost:8080/';
+
 const config = {
+  setupFilesAfterEnv: ['./__tests__/jest.setup.js'],
   rootDir: 'src/',
   testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.spec.js'],
   moduleFileExtensions: [
     'js',
     'json',
@@ -14,6 +21,12 @@ const config = {
     '.*\\.(vue)$': 'vue-jest',
     '.*\\.(js)$': 'babel-jest',
   },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '.*\\.(svg)$': '<rootDir>/tests/__mocks__/fileMock.js',
+  },
+  transformIgnorePatterns: [],
+  automock: false,
 };
 
 module.exports = config;
---
+++ src/__tests__/jest.setup.js
@@ -0,0 +1,25 @@
+// jest.setup.js
+//import MockDate from 'mockdate';
+import Vue from 'vue';
+import Buefy from 'buefy';
+
+Vue.use(Buefy);
+
+Vue.config.productionTip = false;
+
+//MockDate.set(1534341842684);
+global.runtimeVariables = {
+  VUE_APP_SUPPORTED_LANGUAGES: 'en|pt',
+  VUE_APP_VERSION: '0.0.0',
+  VUE_APP_OPTIONS_WENIGPT:
+    '{"golfinho-1": "runpod", "boto-1": "ft:gpt-3.5-turbo-0125:weni:wenigpt:9Fpx8jej"}',
+  VUE_APP_BOTHUB_NLP_BASE_URL: 'http://localhost:2657/',
+  VUE_APP_BOTHUB_WEBAPP_BASE_URL: 'http://localhost:8080/',
+  get(name) {
+    return this[name] || process.env[name];
+  },
+};
+
+window.alert = () => {};
+window.matchMedia = () => ({});
+window.scrollTo = () => {};
