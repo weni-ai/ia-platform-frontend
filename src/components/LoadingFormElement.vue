0326667d9b75d1eb3edec718dc1d82a615f68f30
Matheus Cristian <mathues@email.com>
2024-06-26 11:54:04 -0300

    feat(customization-tab-skeleton-loading): adds skeleton loading to customization tab

---
+++ src/components/LoadingFormElement.vue
@@ -0,0 +1,64 @@
+<template>
+  <section
+    class="skeleton-loading"
+    :style="{ marginTop }"
+  >
+    <UnnnicSkeletonLoading
+      v-if="label"
+      class="skeleton-loading__label"
+      tag="div"
+      width="90px"
+      height="22px"
+    />
+
+    <UnnnicSkeletonLoading
+      tag="div"
+      :width="elementWidth"
+      :height="elementHeight"
+    />
+  </section>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  label: Boolean,
+  element: {
+    type: String,
+    default: 'input',
+  },
+  elementHeight: String,
+  elementWidth: {
+    type: String,
+    default: '100%',
+  },
+  marginTop: String,
+});
+
+const defaultElementHeights = {
+  input: '46px',
+  textarea: '104px',
+};
+
+const elementHeight = computed(
+  () => props.elementHeight || defaultElementHeights[props.element],
+);
+</script>
+
+<style lang="scss" scoped>
+@import '@weni/unnnic-system/src/assets/scss/unnnic.scss';
+
+.skeleton-loading {
+  display: block;
+  width: 100%;
+
+  :deep(*) {
+    display: block;
+  }
+
+  &__label {
+    margin-bottom: $unnnic-spacing-xs;
+  }
+}
+</style>
