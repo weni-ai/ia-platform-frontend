fa20a3e8e56bd5521c2e2abe12745d51ee06848f
MarcusViniciusLSantos <marcusvinici25@gmail.com>
2024-07-02 12:44:46 -0300

    adding shared tests

---
+++ src/__tests__/components/shared/SentenceAccordion.spec.js
@@ -0,0 +1,62 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Buefy from 'buefy';
+import SentenceAccordion from '@/components/shared/accordion/SentenceAccordion';
+
+const localVue = createLocalVue();
+localVue.use(Buefy);
+
+describe('SentenceAccordion.vue', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(SentenceAccordion);
+  });
+
+  test('renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('check click should ignore accordion', () => {
+    beforeEach(() => {
+      wrapper = shallowMount(SentenceAccordion, {
+        propsData: {
+          open: false,
+        },
+      });
+      const check = wrapper.findComponent({ ref: 'check' });
+      check.trigger('click');
+    });
+    test('expand should be false', () => {
+      expect(wrapper.vm.isOpen).toBeFalsy();
+    });
+  });
+
+  describe('open accordion header', () => {
+    beforeEach(() => {
+      wrapper = shallowMount(SentenceAccordion, {
+        propsData: {
+          open: false,
+        },
+      });
+      const colapseButton = wrapper.findComponent({ ref: 'expander' });
+      colapseButton.trigger('click');
+    });
+    test('Expand should be true', () => {
+      expect(wrapper.vm.isOpen).toBeTruthy();
+    });
+  });
+
+  describe('close accordion header', () => {
+    beforeEach(() => {
+      wrapper = shallowMount(SentenceAccordion, {
+        propsData: {
+          open: true,
+        },
+      });
+      const colapseButton = wrapper.findComponent({ ref: 'expander' });
+      colapseButton.trigger('click');
+    });
+    test('Expand should be true', () => {
+      expect(wrapper.vm.isOpen).toBeFalsy();
+    });
+  });
+});
