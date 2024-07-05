5a2814ef2caf16a9d27dd8fbdfca9ba96dcbd37e
Marcus Vinícius <marcusvinici25@gmail.com>
2024-07-05 15:00:57 -0300

    removing default test

--- src/__tests__/HelloVitest.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils';
-import HelloWorld from '../components/HelloVitest.vue';
-
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { propsData: { msg: 'Hello Vitest' } });
-    expect(wrapper.text()).toContain('Hello Vitest');
-  });
-});
