79bb6190666824a4d466c56eb1b1dbb90ec94e73
Matheus Cristian <mathues@email.com>
2024-07-24 18:47:29 -0300

    feat(redesign-action-creation): adds nominate action step

---
+++ src/components/actions/steps/__tests__/NominateAction.unit.spec.js
@@ -0,0 +1,22 @@
+import { mount } from '@vue/test-utils';
+import NominateAction from '../NominateAction.vue';
+
+describe('ModalActions.vue', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(NominateAction, {
+      name: 'Name',
+    });
+  });
+
+  describe('when the user changes the name input', () => {
+    it('emits update:name', () => {
+      const input = wrapper.findComponent({ name: 'UnnnicInput' });
+
+      input.vm.$emit('update:modelValue', 'Action Name');
+
+      expect(wrapper.emitted('update:name')).toContainEqual(['Action Name']);
+    });
+  });
+});
