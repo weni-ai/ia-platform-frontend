fc52503af35d730ba13e23902a739a61eff3bfd9
Marcus Vinícius <marcusvinici25@gmail.com>
2024-07-24 19:38:27 -0300

    removing  internal function in nexusaiAPI

--- src/api/nexusaiAPI.js
+++ src/api/nexusaiAPI.js
@@ -1,58 +1,5 @@
-import axios from 'axios';
-import * as Sentry from '@sentry/browser';
-import store from '../store';
-import i18n from '../utils/plugins/i18n';
-import { get } from 'lodash';
-
-const request = {
-  get $http() {
-    const client = axios.create({
-      baseURL: runtimeVariables.get('NEXUS_API_BASE_URL'),
-      headers: {
-        ...(store.getters.authenticated
-          ? { Authorization: `${store.getters.authToken}` }
-          : {}),
-      },
-    });
-    client.interceptors.response.use(
-      (res) => res,
-      (err) => {
-        Sentry.captureException(err);
-
-        if (get(err, 'config.hideGenericErrorAlert')) {
-          throw err;
-        }
-
-        const statusCode = get(err, 'response.status');
-        let defaultMessage;
-
-        if ([500, 408].includes(statusCode)) {
-          defaultMessage = i18n.global.t('internal_server_error');
-        } else if (statusCode === 401) {
-          defaultMessage = i18n.global.t('unauthorized');
-        } else {
-          defaultMessage = i18n.global.t('unexpected_error');
-        }
-
-        store.state.alert = {
-          text: get(err, 'response.data.detail') || defaultMessage,
-          type: 'error',
-        };
-
-        throw err;
-      },
-    );
-    return client;
-  },
-};
-
-function forceHttps(receivedUrl) {
-  const url = new URL(receivedUrl);
-
-  url.protocol = 'https:';
-
-  return url.toString();
-}
+import request from '@/api/nexusaiRequest';
+import forceHttps from '@/api/utils/forceHttps';
 
 export default {
   question: {
@@ -359,7 +306,6 @@ export default {
       files: {
         create({ contentBaseUuid, file, extension_file, onUploadProgress }) {
           const form = new FormData();
-
           const fileName =
             file.name.lastIndexOf('.') === -1
               ? file.name
