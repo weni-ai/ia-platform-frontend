c2edd0f2b9979578b1a95ba5db55812e241e32bd
Matheus Cristian <mathues@email.com>
2024-08-03 13:10:47 -0300

    test(IntegrateNotification.vue): adds unit test

---
+++ src/components/repository/IntegrateNotification.vue
@@ -0,0 +1,34 @@
+<template>
+  <UnnnicModal
+    showModal
+    :text="title"
+    :scheme="type === 'success' ? 'feedback-green' : 'feedback-red'"
+    :modalIcon="type === 'success' ? 'check-circle-1-1' : 'alert-circle-1'"
+    @close="$emit('close')"
+  >
+    <template #message>
+      <span v-html="message" />
+    </template>
+  </UnnnicModal>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  type: {
+    type: String,
+    required: true,
+  },
+
+  message: {
+    type: String,
+    required: true,
+  },
+});
+
+defineEmits(['close']);
+</script>
