d5412d0dfdf9d88d3a34565057b8a693c9a44144
Marcus Vinícius <marcusvinici25@gmail.com>
2024-08-07 11:05:25 -0300

    adding watchOnce unit test

---
+++ src/utils/__test__/watchOnce.spec.js
@@ -0,0 +1,22 @@
+// watchOnce.test.js
+import { describe, it, vi, expect } from 'vitest';
+import watchOnce from '@/utils/watchOnce';
+import store from '@/store/index.js';
+
+describe('watchOnce', () => {
+  it('should call the provided callback and unwatch', () => {
+    const unwatchMock = vi.fn();
+    store.watch = vi.fn(() => unwatchMock);
+
+    const mockGetter = vi.fn();
+    const mockCallback = vi.fn();
+
+    watchOnce(mockGetter, mockCallback);
+
+    store.watch.mock.calls[0][1]();
+
+    expect(mockCallback).toHaveBeenCalled();
+
+    expect(unwatchMock).toHaveBeenCalled();
+  });
+});
