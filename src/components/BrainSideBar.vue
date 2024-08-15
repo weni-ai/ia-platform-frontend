ded919007fd3041e12f850746e2822fc7129211f
MarcusViniciusLSantos <marcusvinici25@gmail.com>
2024-08-15 18:26:37 -0300

    adding btn floating logic

--- src/components/BrainSideBar.vue
+++ src/components/BrainSideBar.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="sidebar">
+    <UnnnicButton
+      class="floating-button"
+      iconCenter="arrow_back_ios_new"
+      type="secondary"
+      @click="handleButtonClick"
+    />
     <UnnnicSideBar expanded>
       <UnnnicSidebarMenu>
         <UnnnicSidebarItem
@@ -38,18 +44,61 @@ const onTabChange = (pageName) => {
     router.push({ name: pageName });
   }
 };
+
+const handleButtonClick = () => {
+  console.log('Floating button clicked');
+};
 </script>
 
 <style lang="scss" scoped>
 .sidebar {
   border-style: solid;
   border-color: $unnnic-color-neutral-soft;
-  border-width: $unnnic-border-width-thinner 0 0 $unnnic-border-width-thinner;
+  border-width: 0 0 0 $unnnic-border-width-thinner;
   padding: $unnnic-spacing-sm;
   width: 182px;
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
+  position: relative;
+
+  .floating-button {
+    position: absolute;
+    top: 27px;
+    left: 100%;
+    transform: translateX(-50%);
+    border-radius: $unnnic-border-radius-pill;
+    background: $unnnic-color-neutral-white;
+    border: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+    padding: 0;
+    opacity: 0;
+    transition: opacity 0.3s ease;
+
+    &:hover {
+      background-color: darken(blue, 10%);
+    }
+  }
+
+  &:hover .floating-button {
+    opacity: 1;
+  }
+
+  :deep(.unnnic-button--icon-on-center.unnnic-button--size-large) {
+    width: 18px;
+    height: 18px;
+    padding: 0;
+    border: 1px solid $unnnic-color-neutral-clean;
+  }
+
+  :deep(.material-symbols-rounded.unnnic-icon-size--md) {
+    font-size: 10px;
+    color: $unnnic-color-neutral-clean;
+  }
 
   :deep(.unnnic-side-bar-item) {
     &:hover {
