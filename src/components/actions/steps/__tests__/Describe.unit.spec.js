0de91cc3267ae2baeedaeee48322bb07e7a085c5
Marcus Vinícius <marcusvinici25@gmail.com>
2024-08-16 12:24:34 -0300

    adding new describe tests

--- src/components/actions/steps/__tests__/Describe.unit.spec.js
+++ src/components/actions/steps/__tests__/Describe.unit.spec.js
@@ -9,6 +9,7 @@ describe('Describe.vue', () => {
       props: {
         description: 'Initial Description',
         actionType: 'normal',
+        currentActions: [{ actionType: 'normal' }, { actionType: 'cart' }],
       },
     });
   });
@@ -18,7 +19,6 @@ describe('Describe.vue', () => {
       const textarea = wrapper.findComponent(
         '[data-test="description-textarea"]',
       );
-
       await textarea.vm.$emit('update:modelValue', 'Updated Description');
 
       expect(wrapper.emitted('update:description')).toContainEqual([
@@ -30,10 +30,51 @@ describe('Describe.vue', () => {
   describe('when the user selects a different action type', () => {
     it('emits update:actionType', async () => {
       const radios = wrapper.findAllComponents('[data-test="radio-option"]');
-
       await radios[1].vm.$emit('update:modelValue', 'cart');
 
       expect(wrapper.emitted('update:actionType')).toContainEqual(['cart']);
+    });
+  });
+
+  describe('hasCartAction function', () => {
+    it('returns true if currentActions contains actionType "cart"', () => {
+      expect(wrapper.vm.hasCartAction()).toBe(true);
+    });
+
+    it('returns false if currentActions does not contain actionType "cart"', async () => {
+      await wrapper.setProps({ currentActions: [{ actionType: 'normal' }] });
+      expect(wrapper.vm.hasCartAction()).toBe(false);
+    });
+  });
+
+  describe('conditional rendering and behavior of radio buttons', () => {
+    it('disables the "cart" radio button when hasCartAction is true', async () => {
+      const radios = wrapper.findAllComponents('[data-test="radio-option"]');
+      expect(radios[1].props('disabled')).toBe(true);
+    });
+
+    it('enables the "cart" radio button when hasCartAction is false', async () => {
+      await wrapper.setProps({ currentActions: [{ actionType: 'normal' }] });
+      const radios = wrapper.findAllComponents('[data-test="radio-option"]');
+      expect(radios[1].props('disabled')).toBe(false);
+    });
+
+    it('enables the UnnnicToolTip for the "cart" radio button when hasCartAction is true', async () => {
+      const radios = wrapper.findAllComponents('[data-test="radio-option"]');
+      const cartRadio = radios[1];
+      const tooltip = cartRadio.findComponent('[data-test="radio-tooltip"]');
+      expect(tooltip.exists()).toBe(true);
+      expect(tooltip.props('enabled')).toBe(true);
+    });
+
+    it('disables the UnnnicToolTip for the "cart" radio button when hasCartAction is false', async () => {
+      await wrapper.setProps({ currentActions: [{ actionType: 'normal' }] });
+      const radios = wrapper.findAllComponents('[data-test="radio-option"]');
+      const cartRadio = radios[1];
+      const tooltip = cartRadio.findComponent('[data-test="radio-tooltip"]');
+
+      expect(tooltip.exists()).toBe(true);
+      expect(tooltip.props('enabled')).toBe(false);
     });
   });
 
