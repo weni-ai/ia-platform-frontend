2e93ce99d7b291c1b0ed23b00d4df86317032d0d
Marcus Vinícius <marcusvinici25@gmail.com>
2024-08-20 16:09:34 -0300

    adding BrainWarning component

---
+++ src/components/Brain/BrainWarningBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="news-bar news-bar--warn">
+    <UnnnicIntelligenceText
+      color="neutral-white"
+      family="secondary"
+      weight="bold"
+      size="body-lg"
+    >
+      {{ $t('router.warn.brain_is_deactivated') }}
+
+      <RouterLink
+        :to="{
+          name: 'router-tunings',
+          query: { activate_brain: true },
+        }"
+      >
+        {{ $t('router.warn.click_here') }}
+      </RouterLink>
+
+      {{ $t('router.warn.activate_it') }}
+    </UnnnicIntelligenceText>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.news-bar {
+  padding: $unnnic-spacing-ant $unnnic-spacing-lg;
+
+  &--warn {
+    background-color: $unnnic-color-aux-yellow-500;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: underline;
+    text-underline-offset: $unnnic-spacing-nano;
+  }
+}
+</style>
