80aaa4b49cb044986ab55675bb1686172336e751
Marcus Vinícius <marcusvinici25@gmail.com>
2024-08-22 18:34:52 -0300

    adding new logic to Describe

--- src/components/actions/steps/Describe.vue
+++ src/components/actions/steps/Describe.vue
@@ -1,15 +1,21 @@
 <template>
-  <section class="describe-container">
+  <section
+    class="describe-container"
+    :class="{ 'whatsapp-cart-active': actionType === 'whatsapp_cart' }"
+  >
     <UnnnicFormElement
+      class="labels-textarea"
       :label="$t('modals.actions.add.steps.describe.inputs.description.label')"
       :message="$t('modals.actions.add.steps.describe.inputs.description.help')"
     >
       <UnnnicTextArea
         v-model="description"
+        class="text-area"
         :placeholder="
           $t('modals.actions.add.steps.describe.inputs.description.placeholder')
         "
         data-test="description-textarea"
+        :disabled="actionType === 'whatsapp_cart'"
       />
     </UnnnicFormElement>
 
@@ -23,14 +29,14 @@
         :value="option.value"
         size="md"
         data-test="radio-option"
-        :disabled="hasCartAction() && option.value === 'cart'"
+        :disabled="hasCartAction() && option.value === 'whatsapp_cart'"
       >
         <UnnnicToolTip
           side="top"
           :text="
             $t('modals.actions.add.steps.describe.inputs.radio.disable_message')
           "
-          :enabled="hasCartAction() && option.value === 'cart'"
+          :enabled="hasCartAction() && option.value === 'whatsapp_cart'"
           data-test="radio-tooltip"
         >
           {{ option.text }}
@@ -41,7 +47,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import i18n from '@/utils/plugins/i18n.js';
 
 const description = defineModel('description', {
@@ -65,23 +71,35 @@ const props = defineProps({
 });
 
 const hasCartAction = () => {
-  return props.currentActions.some((action) => action.actionType === 'cart');
+  return props.currentActions.some(
+    (action) => action.actionType === 'whatsapp_cart',
+  );
 };
 
 const flowOptions = ref([
   {
-    value: 'normal',
+    value: 'custom',
     text: i18n.global.t(
       'modals.actions.add.steps.describe.inputs.radio.options.normal',
     ),
   },
   {
-    value: 'cart',
+    value: 'whatsapp_cart',
     text: i18n.global.t(
       'modals.actions.add.steps.describe.inputs.radio.options.cart',
     ),
   },
 ]);
+
+watch(actionType, (newVal) => {
+  if (newVal === 'whatsapp_cart') {
+    description.value = i18n.global.t(
+      'modals.actions.add.steps.describe.inputs.description.aux_text_whatsapp_cart',
+    );
+  }
+
+  if (newVal === 'custom') description.value = '';
+});
 </script>
 
 <style lang="scss" scoped>
@@ -89,5 +107,19 @@ const flowOptions = ref([
   display: flex;
   flex-direction: column;
   gap: $unnnic-spacing-md;
+
+  &.whatsapp-cart-active {
+    .labels-textarea {
+      :deep(.label),
+      :deep(.message) {
+        color: $unnnic-color-neutral-cleanest;
+      }
+    }
+
+    :deep(.unnnic-text-area textarea) {
+      color: $unnnic-color-neutral-cleanest;
+      background-color: $unnnic-color-neutral-lightest;
+    }
+  }
 }
 </style>
