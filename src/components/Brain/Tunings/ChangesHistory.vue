ae1714370f2800899af7454e32b8bcbad7239220
Marcus Vinícius <marcusvinici25@gmail.com>
2024-09-11 18:47:22 -0300

    adding initial logic to ChangesHistory

---
+++ src/components/Brain/Tunings/ChangesHistory.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <h1>Changes History</h1>
+    <UnnnicTableNext
+      v-model:pagination="pagination"
+      :headers="table.headers"
+      :rows="table.rows"
+      :paginationTotal="125"
+      :paginationInterval="5"
+    />
+  </section>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const pagination = ref(1);
+
+const table = ref({
+  headers: [{ content: 'Alterações', size: 6 }, { content: 'Data' }],
+  rows: [
+    {
+      content: ['1', 'Alice'],
+    },
+    {
+      content: ['2', 'Bob'],
+    },
+    {
+      content: ['3', 'Charlie'],
+    },
+    {
+      content: ['4', 'Diana'],
+    },
+    {
+      content: ['5', 'Ethan'],
+    },
+  ],
+});
+</script>
+
+<style scoped></style>
