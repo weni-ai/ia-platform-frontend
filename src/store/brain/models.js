2212c9693c499c4c49d748cba2128198b56d7249
Marcus Vinícius <marcusvinici25@gmail.com>
2024-09-11 11:44:57 -0300

    adding new model gpt-4o-retail

--- src/store/brain/models.js
+++ src/store/brain/models.js
@@ -23,7 +23,13 @@ export const models = [
         type: 'select',
         name: 'version-gpt',
         default: 'gpt-4o',
-        options: ['gpt-3.5-turbo', 'gpt-4-turbo', 'gpt-4o', 'gpt-4o-mini'],
+        options: [
+          'gpt-3.5-turbo',
+          'gpt-4-turbo',
+          'gpt-4o',
+          'gpt-4o-mini',
+          'ft:gpt-4o-2024-08-06:weni:wenigpt:9yfJAg4g',
+        ],
       },
       {
         type: 'password',
