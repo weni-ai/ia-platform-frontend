af12442cc2b91c700bb78b075978fc8f0d62b25b
MarcusViniciusLSantos <marcusvinici25@gmail.com>
2024-09-23 00:24:38 -0300

    creating Brain sidebar

---
+++ src/components/Sidebar/SidebarMenu.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="unnnic-side-bar-menu">
+    <p class="unnnic-side-bar-menu__title">{{ text }}</p>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.unnnic-side-bar-menu {
+  color: $unnnic-color-neutral-cloudy;
+  font-family: $unnnic-font-family-secondary;
+  font-weight: $unnnic-font-weight-regular;
+  margin-bottom: $unnnic-spacing-stack-md;
+
+  &__title {
+    margin: 0 0 $unnnic-spacing-stack-xs 0;
+    font-size: $unnnic-font-size-body-md;
+    line-height: $unnnic-font-size-body-md + $unnnic-line-height-md;
+  }
+}
+</style>
